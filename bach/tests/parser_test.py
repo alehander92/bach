5d7473f6cee0d5dea37e196de7723eb9bf04cf0b
Alexander Ivanov <alehander42@gmail.com>
2014-11-22 19:19:42 +0200

    Add tests for parsing literals

--- bach/tests/parser_test.py
+++ bach/tests/parser_test.py
@@ -9,10 +9,20 @@ class TestParser(object):
         assert_not_equal(parser.target, '3')
 
     def test_parse_labels(self):
-        ast = Parser('l\nm?\nweird-name\nname_я').parse()
+        ast = Parser(['l', 'm?', 'weird-name', 'name_я']).parse()
         assert_is_instance(ast.body[0], bach_ast.Label)
         assert_equal(ast.body[0].label, 'l')
         assert_equal(ast.body[1].label, 'm?')
         assert_equal(ast.body[2].label, 'weird-name')
         assert_equal(ast.body[3].label, 'name_я')
 
+    def test_parse_booleans(self):
+        ast = Parser(['#t', '#f']).parse()
+        assert_equal(ast.body[0].value, True)
+        assert_equal(ast.body[1].value, False)
+    
+    def test_parse_numbers(self):
+        ast = Parser(['2', '-0.3', '2.2']).parse()
+        assert_equal(ast.body[0].value, 2)
+        assert_equal(ast.body[1].value, -0.3)
+        assert_equal(ast.body[2].value, 2.2)
