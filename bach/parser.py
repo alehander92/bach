ff4799fec2fc2303974f1b16f3424d9fe4140ca3
Alexander Ivanov <alehander42@gmail.com>
2014-11-24 22:00:01 +0200

    Support comments

--- bach/parser.py
+++ bach/parser.py
@@ -3,9 +3,12 @@ import grammar, converter
 class Parser(object):
     def __init__(self, source):
         if isinstance(source, list):
-            source = source.join('\n')
-
-        self.source = source
+        	lines = source
+        else:
+        	lines = source.split('\n')
+        lines = [line for line in lines if not line.startswith(';')]
+        print(lines, source)
+        self.source = '\n'.join(lines)
 
     def parse(self):
         # invoke the parser generator
